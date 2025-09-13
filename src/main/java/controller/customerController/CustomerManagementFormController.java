c1ab3655008ba0ce895eae66e0bae3e2b81a092c
AyeshaS1031 <ayeshasiddeeqa1031@gmail.com>
2025-09-13 09:55:28 +0530

    customer UI and logics completed;

---
+++ src/main/java/controller/customerController/CustomerManagementFormController.java
@@ -0,0 +1,81 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CustomerManagementFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableView<?> tblCustomers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtContact;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtName;
+
+    DashboardFormController
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnManageItems(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnManageOrderDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnManageOrders(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
